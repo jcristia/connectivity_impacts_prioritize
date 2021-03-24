82c0e464cda84eb25c7840c157f6e025e18a739e
jcristiani <jcristia10@gmail.com>
2021-03-24 14:12:08 -0700

    initial setup

---
+++ scripts/rank_prioritize/201_prioritizr_prep.py
@@ -0,0 +1,104 @@
+# prepare data inputs for prioritizr
+# see Impacts\scripts\prioritizr_seagrass\prioritizr_scenarios.xlsx
+
+import arcpy
+import os
+
+root = r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Impacts\spatial'
+gdb = 'prioritizr.gdb'
+arcpy.env.workspace = os.path.join(root, gdb)
+sg = os.path.join(root, 'main_seagrass.gdb/sg_5_join_norm')
+
+# copy to prioritizr gdb
+arcpy.CopyFeatures_management(sg, 'sg_01_copy')
+
+# add area attribute and calculate
+arcpy.AddField_management('sg_01_copy', 'area', 'FLOAT')
+with arcpy.da.UpdateCursor('sg_01_copy', ['Shape_Area', 'area']) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        cursor.updateRow(row)
+
+# convert to points
+arcpy.FeatureToPoint_management('sg_01_copy', 'sg_02_pt', 'INSIDE')
+
+# Add fields
+arcpy.AddFields_management(
+    'sg_02_pt',
+    [
+        ['id', 'SHORT', 'id'],
+        ['cost1', 'LONG', 'cost1'],
+        ['cost2', 'LONG', 'cost2'],
+        ['locked_in', 'TEXT', 'locked_in', 50],
+        ['locked_out', 'TEXT', 'locked_out', 50],
+        ['dummy_con', 'SHORT', 'dummy_con'],
+        ['area_scaled', 'FLOAT', 'area_scaled']
+    ]
+)
+
+# Calc fields:
+
+# id = uID
+# cost1 = area
+# cost2 = 1
+# dummy_con = 1
+fields = [
+    'uID', 'id',
+    'area', 'cost1', 'cost2',
+    'dummy_con'
+    ]
+with arcpy.da.UpdateCursor('sg_02_pt', fields) as cursor:
+    for row in cursor:
+        row[1] = row[0]
+        row[3] = row[2]
+        row[4] = 1
+        row[5] = 1
+        cursor.updateRow(row)
+
+# Locked in/out:
+# I initially just added TRUE if anything overlapped with an MPA:
+# locked_in = TRUE if mpa overlap > 0
+# locked_out = TRUE if mpa overlap > 0
+# However...
+# Some large meadows overlap just a small bit with an mpa. I wouldn't want to
+# exclude these. However, I shouldn't do it just by % overlap because some very
+# small meadows only overlap a bit with an mpa, but given their size and the 
+# mismatch in meadow/mpa boundaries, I would still want to lock these ones out.
+# Therefore, determine a combination overlap/total area criteria to
+# exclude/include meadows.
+# Through some trial and error I concluded: overlap of 20% and meadows greater
+# than 500,000
+fields = [
+    'percent_mpaoverlap', 'area',
+    'locked_in', 'locked_out',
+    ]
+with arcpy.da.UpdateCursor('sg_02_pt', fields) as cursor:
+    for row in cursor:
+        if row[0] > 0:
+            row[2] = 'TRUE'
+            row[3] = 'TRUE'
+        else:
+            row[2] = 'FALSE'
+            row[3] = 'FALSE'
+        if row[0] < 20 and row[0] > 0 and row[1] > 500000:
+            row[2] = 'FALSE'
+            row[3] = 'FALSE'
+        cursor.updateRow(row)
+
+# scale area to 0 to 1
+search_cursor = arcpy.da.SearchCursor('sg_02_pt', 'area')
+max_val = max(search_cursor)[0]
+search_cursor.reset()
+min_val = min(search_cursor)[0]
+with arcpy.da.UpdateCursor('sg_02_pt', ['area', 'area_scaled']) as cursor:
+    for row in cursor:
+        row[1] = (row[0] - min_val) / (max_val - min_val)
+        cursor.updateRow(row)
+
+# Delete fields:
+# uID, area_clipmpa, percent_mpaoverlap, ORIG_FID
+arcpy.DeleteField_management('sg_02_pt', ['uID', 'area_clipmpa', 'percent_mpaoverlap', 'ORIG_FID'])
+
+
+
+
---
+++ scripts/rank_prioritize/202_prioritizr_targets.py
@@ -0,0 +1,93 @@
+# calculate targets for each feature
+
+# First, I need to set an area target, which will guide the connectivity targets.
+# See  'Design Strategies for the NSB MPA network' (draft version) for details.
+# They used a 20-40% target for eelgrass in the NSB.
+# I will get the total area and see what % is alreeady protected and subtract
+# that from the initial target %. This will be my seagrass area target.
+# I can also do a scenario with nothing locked out and the original 40% target.
+
+# Then, I will set connectivity targets.
+# I inititally tried the approach from Magris et al 2017, but it doesn't really
+# make sense (see notes below).
+
+# So for now I will just start with conn metrics at 16% as well. I will test 
+# a range of targets for the conn features and calculate EC(PC) vs. cost.
+# See the post analysis script.
+
+
+import arcpy
+import os
+import pandas as pd
+
+root = r'C:\Users\jcristia\Documents\GIS\MSc_Projects\Impacts\spatial'
+gdb = 'prioritizr.gdb'
+arcpy.env.workspace = os.path.join(root, gdb)
+sg = 'sg_01_copy'
+
+# fc to df
+field_names = [i.name for i in arcpy.ListFields(sg) if i.name not in ('OBJECTID', 'Shape')]
+cursor = arcpy.da.SearchCursor(sg, field_names)
+df = pd.DataFrame(data=[row for row in cursor], columns=field_names)
+
+# AREA
+area_target_initial = 0.40
+total_seagrass_area = df.area.sum()
+total_seagrass_protected = df.area_clipmpa.sum()
+precent_seagrass_protected = total_seagrass_protected / total_seagrass_area
+percent_to_protect = round(area_target_initial - precent_seagrass_protected, 2)
+# this ends up being ~ 16%
+# so when I lock out mpas, use 16%
+# when I run a scenario without any restrictions, use 40%
+
+
+# # ARCHIVING THIS FOR NOW:
+# # CONNECTIVITY
+# # This is based on the methodology from Magris et al 2017.
+# # get the total amount for a column
+# # get the total for the top third(?) of values. So for 653 meadows, this would be
+# # the connectivity of the top ranked 217-8 meadows.
+# # then divide that sum by the overall sum, and that is my target.
+# # So in a way, let's say you want to conserve about 1/3 of seagrass area. Then
+# # If you say that you want to conserve 1/3 of connectivity, since some meadows
+# # dispproportionately contain a lot more connectivity than others, then you
+# # might end up only selecting 5% of meadows to reach that 1/3 of connectivity.
+# cols = ['dPCpld01', 'dPCpld60', 'sink_01', 'sink_60', 'source01', 'source60',
+#        'gpfill01', 'gpfill60']
+
+# # the output for this is simply a dictionary.
+# # I'll just add them in to my R script manually.
+# targets_area_all = {}
+# targets_area_adj = {}
+
+# # for initial % area targets:
+# nrows = round(len(df) * area_target_initial)
+# nrows = 14
+# for col in cols:
+#     # get sum
+#     total = df[col].sum()
+#     # get top % of rows
+#     df_sort = df.sort_values(col, ascending=False).head(nrows)
+#     total_toprows = df_sort[col].sum()
+#     target_col = round(total_toprows / total, 2)
+#     targets_area_all[col] = target_col
+
+# # for reduced % area targets:
+# nrows = round(len(df) * percent_to_protect)
+# for col in cols:
+#     # get sum
+#     total = df[col].sum()
+#     # get top % of rows
+#     df_sort = df.sort_values(col, ascending=False).head(nrows)
+#     total_toprows = df_sort[col].sum()
+#     target_col = round(total_toprows / total, 2)
+#     targets_area_adj[col] = target_col
+
+# # I'll need to do some testing. The high numbers seem alarming, but...
+# # for sink_01, the 110th meadow has a value of 0.009 (on a scale of 0-1).
+# # BUT, if I have a target of 100% then it will just always select those meadows
+# # and I won't see trade offs, hmmmmmmmm.
+# # Now I wonder, does this even make sense? Why not just flat targets? Magris
+# # doesn't cite anything for why they did it this way.
+    
+# # the dictionaries are my output. I can just enter these manually into my R script
